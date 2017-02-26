689185facd5e4b45decf1ef71de008bf5b62eb57
Sam Crouch <chacursmo@gmail.com>
2017-02-26 16:07:57 -0600

    FEATURE: Now this is cooler

--- js/action.js
+++ js/action.js
@@ -1,15 +1,24 @@
 $(document).ready(function(){
-    $.ajax({
-	url:"https://wind-bow.gomix.me/twitch-api/streams/freecodecamp",
-	dataType: "jsonp",
-	success: function(data){
-	    console.log(data);
-	},
-	error: function(data){
-	    alert("money");
-	}
-    });
-    
+    var bogo = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    for (var i = 0; i < bogo.length; i++){
+	var temp = String(bogo[i]);
+	var myurl = "https://wind-bow.gomix.me/twitch-api/streams/"+temp;
+	$(".main").append("<div class='popeye'><a href="+myurl+">"+temp.toUpperCase()+"</></div><br>");
+
+	/*
+	$.ajax({
+	    url:myurl,
+	    dataType: "jsonp",
+	    success: function(data){
+		console.log(temp);
+	    	$(".main").append("<div class='popeye'><a href="+data._links.channel+">"+data._links.channel.slice(38).toUpperCase()+"</></div><br>");
+	    console.log(data._links.channel);
+	    },
+	    error: function(data){
+		alert("money");
+	    }
+	});*/
+    };
 });
 
 
