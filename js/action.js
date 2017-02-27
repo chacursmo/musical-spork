a049dd4b93c1b93ecc02ec86648a257984f49bae
Sam Crouch <chacursmo@gmail.com>
2017-02-27 08:17:39 -0600

    FEATURE all ur users stories R belong to us

--- js/action.js
+++ js/action.js
@@ -1,27 +1,36 @@
 $(document).ready(function(){
-    var bogo = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+    var bogo = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas", "brunofin", "comster404"];
     for (var i = 0; i < bogo.length; i++){
-	var temp = String(bogo[i]);
-	var myurl = "https://wind-bow.gomix.me/twitch-api/streams/"+temp;
-	$(".main").append("<div class='popeye'><a href="+myurl+">"+temp.toUpperCase()+"</></div><br>");
+	buildElements(bogo[i]);
+    }
 
-	/*
-	$.ajax({
-	    url:myurl,
-	    dataType: "jsonp",
-	    success: function(data){
-		console.log(temp);
-	    	$(".main").append("<div class='popeye'><a href="+data._links.channel+">"+data._links.channel.slice(38).toUpperCase()+"</></div><br>");
-	    console.log(data._links.channel);
-	    },
-	    error: function(data){
-		alert("money");
-	    }
-	});*/
-    };
 });
 
 
+function buildElements(name){
+    var myurl = "https://wind-bow.gomix.me/twitch-api/streams/"+name;
+    var theyreurl = "https://twitch.tv/"+name;
+    $.ajax({
+	url:myurl,
+	dataType: "jsonp",
+	success: function(data){
+	    var online = "Offline";
+	    var more = "";
+	    console.log(data);
+	    if (data.stream){
+		online = "Online Yeah!";
+		more = data.stream.game;
+	    }
 
 
+	    var builtHtml = "<div class='popeye'><a target='_blank' href="+theyreurl+">"+name+"</a><br>"+online+"<br>"+more+"</div><br>";
+	    $(".main").append(builtHtml);
 
+	},
+	error: function(data){
+	    alert("something went wrong");
+	}
+    });
+
+
+}
